5e288f0a4037b962efba2fd03965eb8cea3d2d10
ahinsatheesh1 <ahin100123@gmail.com>
2025-08-24 19:11:09 +0530

    Initial commit-Wanderlust project

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate=require("ejs-mate");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+
+main()
+  .then(() => {
+    console.log("✅ Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+// middleware
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+// Root
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+// ================== CRUD Routes ==================
+
+// Index Route - show all listings
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { allListings });
+});
+
+// New Route - form to create new listing
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new");
+});
+
+// Create Route - actually add to DB
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+// Show Route - show a particular listing
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show", { listing });
+});
+
+// Edit Route - form to edit a listing
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit", { listing });
+});
+
+// Update Route - update in DB
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete Route - delete from DB
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log("Deleted:", deletedListing);
+  res.redirect("/listings");
+});
+
+// =================================================
+
+app.listen(8080, () => {
+  console.log("🚀 Server is listening on port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage"
+    },
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1590523278191-995cbcda646b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1590523278191-995cbcda646b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+          : v,
+    }
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
